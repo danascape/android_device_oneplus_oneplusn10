c78d10339eac1aa9fa41e4d181f61eab658de61d
Saalim Quadri <danascape@gmail.com>
2023-12-22 18:21:34 +0530

    oneplusn10: Auto pickup kernel source directory
    Signed-off-by: Saalim Quadri <danascape@gmail.com>
    
    # Conflicts:
    #	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,7 +76,6 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_CONFIG := vendor/billie-perf_defconfig
-TARGET_KERNEL_SOURCE := kernel/oneplus/sm6350
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "qualcomm-hidl"
